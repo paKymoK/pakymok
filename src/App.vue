18c6dd772f81f9532aea386e4159b2d1a59e9b18
TakyPok <thaimeo0210@gmail.com>
2023-10-04 01:07:15 +0700

    Update font to Seaborn

--- src/App.vue
+++ src/App.vue
@@ -8,11 +8,10 @@
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: AegirSeaborn;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
 }
 
 nav {
