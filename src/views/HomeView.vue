be1483af902f3f8399c62c17e54c5253737cf46d
TakyPok <thaimeo0210@gmail.com>
2024-01-02 23:13:02 +0700

    update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,21 @@
 <template>
   <div class="home">
-    <HelloWorld msg="Welcome Guy!" />
+    <HelloWorld msg="DM Giỏi" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import userData from "@/assets/data/user.json";
 
 export default {
   name: "HomeView",
+  data() {
+    return {
+      users: userData,
+    };
+  },
   components: {
     HelloWorld,
   },
--- src/views/SettingView.vue
+++ src/views/SettingView.vue
@@ -4,3 +4,18 @@
     <h2>Powered by ꘝꗇꖦꗱ ꘝꗇꖦꗱ ꖀꖾꕯꖹ ꖀꖾꕯꖹ</h2>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      text: "",
+    };
+  },
+  methods: {
+    updateMessage() {
+      console.log(this.text);
+    },
+  },
+};
+</script>
